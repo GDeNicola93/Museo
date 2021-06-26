3e7e541638f37d83f4e6dbf4b9d391f63d438011
Germán De Nicola <denicolagerman@gmail.com>
2021-06-26 11:58:55 -0300

    Inicio de proyecto

---
+++ Museo-Back/src/main/java/com/museoback/MuseoBack/MuseoBackApplication.java
@@ -0,0 +1,13 @@
+package com.museoback.MuseoBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuseoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuseoBackApplication.class, args);
+	}
+
+}
---
+++ Museo-Back/src/test/java/com/museoback/MuseoBack/MuseoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.museoback.MuseoBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuseoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
